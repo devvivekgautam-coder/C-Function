5b8a65bb86c1840965329929680abb6dda4aefae
devvivekgautam-coder <dev.vivekgautam@gmail.com>
2025-11-05 16:33:23 +0530

    change a programe

--- divsition.c
+++ divsition.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 
-int main () {
-    int num;
-
-    printf("Enter any num : ");
-    scanf("%d", &num);
-
-    if ((num % 3 == 0) && (num % 5 == 0)) {
-        printf("Num is divisable.");
-    }
-    else{
-        printf("Num is not divisable.");
-    }
-
+void checkDivisible();
+    
+int main() {
+    
+    checkDivisible(); 
+    
     return 0;
+}
+
+void checkDivisible() {
+    int num;
+    
+    printf("Enter any number: ");
+    scanf("%d", &num);
+    
+    if ((num % 3 == 0) && (num % 5 == 0)) {
+        printf("Number is divisible by both 3 and 5.\n");
+    } 
+    else 
+    {
+        printf("Number is not divisible by both 3 and 5.\n");
+    }
 }
